e59ffe8e4daa27ccedb898189b4e441016d5d46a
Sourashis Pal <sourashis.pal2008@gmail.com>
2022-02-12 13:57:51 +0530

    Upgrade to module

---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * This module contains the command-line hand cricket game
+ * @author Sourashis Pal
+ */
+module HandCricket {
+	exports in.sourashis.handcricket;
+	exports in.sourashis.handcricket.console;
+	exports in.sourashis.handcricket.game;
+}
